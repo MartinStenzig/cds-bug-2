661cd5efd1ecde092e8e473726902e2d418d5b5f
Martin Stenzig <martin.stenzig@rizing.com>
2023-04-30 13:08:10 -0700

    Simplified test cases

--- test/cases/t1_test.js
+++ test/cases/t1_test.js
@@ -2,14 +2,10 @@ import cds from '@sap/cds/lib/index.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// enable __dirname in ESM
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-let expect = null
-
 describe('Test group 1', function () {
-  // expect is a chai assertion library based on cds test
-  ({ expect } = cds.test(path.join(__dirname, '/../..'), '--profile', 'autotest'))
+  const { expect } = cds.test(path.join(__dirname, '/../..'))
 
   it('test case1', function () {
     const t1 = {}
